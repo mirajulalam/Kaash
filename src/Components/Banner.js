1ce3891c70d91a745422dbf4ebde4c1df91c47d2
mirajulalam <alifmirajulalam@gmail.com>
2022-09-22 12:10:54 -0700

    banner components added

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner = () => {
     return (
         <div className='flex justify-center items-center py-64 bg-black'>
-            <h1 className='text-5xl'>'be visible, appear, shine' :  ka-a-sh</h1>
+            <h1 className=' lg:text-5xl md:text-2xl sm:text-xl text-primary'>'be visible, appear, shine' :  ka-a-sh</h1>
         </div>
     );
 };
